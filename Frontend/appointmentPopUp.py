111204d2ddbf1a859f23b2d35160615426e27c9b
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2025-09-24 14:06:25 +0800

    align top in web appointment

--- Frontend/appointmentPopUp.py
+++ Frontend/appointmentPopUp.py
@@ -53,7 +53,7 @@ class AddAppointmentCard(QWidget):
         self.acceptedWebLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         # declined layout
-        self.declinedWebLayout = self.main_window.scrollAreaWebAppDecined.layout()
+        self.declinedWebLayout = self.main_window.scrollAreaWebAppDeclined.layout()
         self.declinedWebLayout.setSpacing(10)
         self.declinedWebLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
@@ -325,6 +325,7 @@ class AddAppointmentCard(QWidget):
 
             webAppointment_id = appoint["id"]
             card.ReviewButton.clicked.connect(lambda: self.main_window.navigate_to_page(7))
+
         for layout in [self.pendingWebLayout, self.acceptedWebLayout, self.declinedWebLayout]:
             if layout.count() == 0:
                 self.add_empty_label(layout)
