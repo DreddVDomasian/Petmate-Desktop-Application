532607f87b2941f1925fb1c6dd9864c356df5184
rmmln <rmmoleno1@gmail.com>
2025-10-05 16:38:32 +0800

    Add appointment form

--- Web Appointment/js/client.js
+++ Web Appointment/js/client.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const clientForm = document.querySelector(".client-form");
   const petForm = document.querySelector(".pet-form");
+  const appointmentForm = document.querySelector(".appointment-form");
 
   const addPetsBtn = document.getElementById("addpets");
   const settingsBtn = document.getElementById("settings");
   const logoutBtn = document.getElementById("logout");
+  const appointmentBtn = document.getElementById("addclient");
+  
+
+  // Hide appointment form by default
+  appointmentForm.style.display = "none";
 
   // Hide pet form by default
   petForm.style.display = "none";
@@ -20,11 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
     petForm.style.display = "none";
     clientForm.style.display = "block";
   });
+  // Show Appointment form
+  appointmentBtn.addEventListener("click", () => {
+    clientForm.style.display = "none";
+    petForm.style.display = "none";
+    appointmentForm.style.display = "block";
+  });
 
   // Logout (for now just alert)
   logoutBtn.addEventListener("click", () => {
     alert("You have logged out.");
     // You can redirect to login page later:
     // window.location.href = "login.html";
+  
+  
   });
 });
