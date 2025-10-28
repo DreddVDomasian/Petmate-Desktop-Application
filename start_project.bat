811ee734c79858e73c77d828cdf42676fed5f0ac
FrancisJerusalem <181956748+JohnnyCodeJourney@users.noreply.github.com>
2025-10-28 20:27:15 +0800

    Update start script to activate virtual environment
    Modified start_project.bat to activate the Python virtual environment before running Django and PyQt applications. The PyQt app launch command is now commented out, likely for development or debugging purposes.

--- start_project.bat
+++ start_project.bat
@@ -2,11 +2,11 @@
 
 :: Start Django Backend
 
-start cmd /k "cd Desktop_Application && cd Backend && python manage.py runserver"
+start cmd /k "cd venv\Scripts && call activate && cd ..\.. && cd Desktop_Application && cd Backend && python manage.py runserver"
 
 
 :: Start PyQt Desktop App
-start cmd /k "cd Desktop_Application && cd Frontend && python app.py"
+::start cmd /k "cd venv\Scripts && call activate && cd ..\.. && cd Desktop_Application && cd Frontend && python app.py"
 
 :: Start React Frontend
 start cmd /k "cd web-applications && npm run dev"
