fba015fc9bfec48f0f156b9e3de8f1545a45ff0b
FrancisJerusalem <181956748+JohnnyCodeJourney@users.noreply.github.com>
2025-12-11 13:57:17 +0800

    change ng password ( 2FA on )

--- Desktop_Application/Backend/myproject/settings.py
+++ Desktop_Application/Backend/myproject/settings.py
@@ -88,7 +88,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'petmateanimalclinic@gmail.com'   # <-- your Gmail address 
-EMAIL_HOST_PASSWORD = 'seqwqhnoohgsgkpi'   # <-- 16-character app password NO SPACE Dapat   #petmate
+EMAIL_HOST_PASSWORD = 'ivxkcgaikytqygwo'   # <-- 16-character app password NO SPACE Dapat   #petmate
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
