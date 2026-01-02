f329f88cdb5cf6929e07b6868e262845c7c3c2ae
FrancisJerusalem <181956748+JohnnyCodeJourney@users.noreply.github.com>
2026-01-03 00:39:37 +0800

    asd
    dds

--- Web-Applications/vite.config.js
+++ Web-Applications/vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  },
+  base: '/', // Change this if deploying to a subdirectory
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
